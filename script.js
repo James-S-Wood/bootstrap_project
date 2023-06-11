eee539af82e50f942014c9c23d3d6755f0e02018
James Wood <james.s.wood@icloud.com>
2023-06-11 00:08:12 -0400

    Reworked the entire application.

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // Custom script
 console.log("Custom script loaded.");
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
